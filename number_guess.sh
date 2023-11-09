e3d4034b591b12f1c464cd190760c7f3923b2535
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-11-09 13:47:36 -0800

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess --no-align --tuples-only
+
+echo "Enter your username:"
+read username
+ NAME=$($PSQL "SELECT username FROM users WHERE username = '$username'");
+  if [[ -$NAME ]] then;
+   INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+   echo "Welcome, $USERNAME ! It looks like this is your first time here."
+   else
+   echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game
+  fi
+
+RANDOM_NUM=$((1 + $RANDOM % 1000))
+echo $RANDOM_NUM 
